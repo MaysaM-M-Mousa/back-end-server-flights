9754e294b75b019d44b6818ae8cb18990f1c4679
MaysaM-M-Mousa <54291847+MaysaM-M-Mousa@users.noreply.github.com>
2022-05-08 12:16:45 +0300

    downloading cors module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const applicationRouter = require('./routes/flight')
 const userRouter = require('./routes/user')
 const predictionRouter = require('./routes/prediction')
 
 const app = express()
-const PORT = process.env.PORT 
+const PORT = process.env.PORT
 
+app.use(cors())
 app.use(express.json())
 app.use(applicationRouter)
 app.use(userRouter)
